d627775f7c0c762c63d08c366ced5e6b671bfef6
James <james.71@hotmail.fr>
2020-11-06 22:13:37 +0100

    Adding Space Invaders

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-const tl = gsap.timeline({defaults: {ease: "power1.out"} });
+const slider = document.querySelector(".slider");
+const hero = document.querySelector(".horizontal");
 
-tl.to('.text', {y:"0%", duration: 1, stagger: 0.25});
-tl.to(".slider", {y: "-100%", duration: 1.5});
-tl.to(".intro", {y: "-100%", duration: 1}, "-=1");
+const tl = new TimelineMax();
+
+tl.fromTo(hero, 1, {height: "0"}, {height: "100%", ease: Power2.easeInOut})
+.fromTo(hero,1.2, {widh: "100%"}, {widh: "100%", ease: Power2.easeInOut})
+
+.fromTo(slider,1.2, {x: "-100%"}, {x: "0%", ease: Power2.easeInOut})
